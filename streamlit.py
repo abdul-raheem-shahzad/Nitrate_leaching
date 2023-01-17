38c6a0165dfe7bbb518e6db2ff70caf1482fb646
Abdul Raheem Shahzad <85006885+abdul-raheem-shahzad@users.noreply.github.com>
2023-01-17 19:13:24 +0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -20,20 +20,19 @@ st.header("Machine Learning and Deep Learning Based Nitrate Leaching Prediction 
 # measurement_depth_m = st.slider('Select the measurement depth (m)', 0, 10)
 # fertilizer_rate_kg = st.slider('Select the fertilizer rate (kg N/ha)', 0, 100)
 
-#set default value of soc_per
-default_value = 6.7
-soc_per = st.number_input('Enter the soil organic carbon (%) 0 ~ 13.5',default=default_value)
-default_value = 11.3
+min_value_value = 6.7
+soc_per = st.number_input('Enter the soil organic carbon (%) 0 ~ 13.5',min_value=min_value_value)
+min_value_value = 11.3
 
-som_per = st.number_input('Enter the soil organic matter (%) 0 ~ 23.22',default=default_value)
-default_value = 0.5
-soil_total_N = st.number_input('Enter the soil total N 0 ~ 0.95',default=default_value)
-default_value = 500
-mean_rainfal_mm = st.number_input('Enter the mean rainfall (mm) 170 ~ 2500',default=default_value)
-default_value = 0.5
-measurement_depth_m = st.number_input('Enter the measurement depth (m) 0.25 ~ 2.40',default=default_value)
-default_value = 400
-fertilizer_rate_kg = st.number_input('Enter the fertilizer rate (kg N/ha) 0 ~ 1800',default=default_value)
+som_per = st.number_input('Enter the soil organic matter (%) 0 ~ 23.22',min_value=min_value_value)
+min_value_value = 0.5
+soil_total_N = st.number_input('Enter the soil total N 0 ~ 0.95',min_value=min_value_value)
+min_value_value = 500
+mean_rainfal_mm = st.number_input('Enter the mean rainfall (mm) 170 ~ 2500',min_value=min_value_value)
+min_value_value = 0.5
+measurement_depth_m = st.number_input('Enter the measurement depth (m) 0.25 ~ 2.40',min_value=min_value_value)
+min_value_value = 400
+fertilizer_rate_kg = st.number_input('Enter the fertilizer rate (kg N/ha) 0 ~ 1800',min_value=min_value_value)
 
 
 
